9d0087d2fc5660201c41d1554e9ce19f252c7286
glik0615 <glik0615@tutamail.com>
2025-05-19 13:10:33 +0300

    the third

--- ex.py
+++ ex.py
@@ -1,22 +1,30 @@
 import pandas as pd
 import os
 
-
-
-
 # Создание класса
 class Payment:
     def __init__(self):
         pass
 
     # Создание метода для обработки и фильтра данных файла
-    def processing(self):
-        # Считывание основного файла
-        df = pd.read_csv('var5.csv')
+    def processing1(self):
         try:
             with open('var5.svg', 'r') as file:
                 print(file.read())
-        except FileNotFoundError as Errno2:
+        except FileNotFoundError :
             print("Возникла следующая ошибка: [Errno2] No such file or directory")
-        except 
-       
+    
+    def processing2(self):
+        try:
+            with open('var5','r') as file:
+                print(file.read())
+        except FileNotFoundError:
+            print('Возникла следующая ошибка:файл пуст')
+
+    def processing3(self):
+        try:
+            with open('var5','r') as file:
+                print(file.read())
+                
+        except:
+            print('Возникла следующая ошибка:файл пуст')
