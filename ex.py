e2dcb8cc5f5498790a960dd8172ad9f54572e0ba
glik0615 <glik_p1627@tutamail.com>
2025-05-22 22:16:07 +0300

    result

--- ex.py
+++ ex.py
@@ -1,30 +1,89 @@
-import pandas as pd
-import os
+# var5 - настоящий файл
+# var4 - пустой файл
+# var3 - неверный файл (структура)
 
-# Создание класса
+import pandas as pd
+from pandas.errors import EmptyDataError
+print('Значения ошибок:')
+print('er1: Файл не обнаружен')
+print('er2: Файл пустой')
+print('er3: Содержимое файла не совпадает с оригиналом')
+print('er4: Расщирение не соответствует формату файла')
+
+# Создаем класс, обрабвтывающий файл
 class Payment:
     def __init__(self):
         pass
-
-    # Создание метода для обработки и фильтра данных файла
+    
+    # Создание метода для проверки наличия файла
     def processing1(self):
         try:
-            with open('var5.svg', 'r') as file:
+            with open('var5.csv', 'r') as file:
                 print(file.read())
-        except FileNotFoundError :
-            print("Возникла следующая ошибка: [Errno2] No such file or directory")
+        except FileNotFoundError as er1:
+            print("Возникла следующая ошибка:{er1}")
+        
+        else:
+            print("Ошибки не обнаружено")
     
+    # Создание метода для проверки файла на наличие содержимого
     def processing2(self):
         try:
             with open('var5','r') as file:
-                print(file.read())
-        except FileNotFoundError:
-            print('Возникла следующая ошибка:файл пуст')
+                pd.read_csv(file)
+        except EmptyDataError as er2:
+            print('Возникла следующая ошибка:{er2}')
 
+
+    # Создание файла, который сравнит полученный датасет с оригиналом(Проверка названий столбцов)
     def processing3(self):
+        # Читаем файлы и заносим названия столбцов в список
+        orig = pd.read_csv('var5.csv')
+        my_file = pd.read_csv('file')
+        column_names_list_orig = orig.columns.tolist()
+        column_names_list = my_file.columns.tolist()
+        # Сравниваем списки
+        if column_names_list_orig==column_names_list:
+            print('Проверка на er3. Названия столбцов совпадают')
+
         try:
-            with open('var5','r') as file:
-                print(file.read())
-                
-        except:
-            print('Возникла следующая ошибка:файл пуст')
+            column_names_list_orig==column_names_list
+        except ValueError as er3:
+            print('Возникла следующая ошибка:{er3}'
+            ' Ожидалось:{column_names_list_orig}. Сравниваемый:{column_names_list}')
+
+    # Создание файла, который сравнит полученный датасет с оригиналом(Проверка типов данных столбцов)
+    def processing4(self):
+            orig = pd.read_csv('var5.csv')
+            my_file = pd.read_csv('file')
+            orig_types = orig.dtypes
+            my_file_types = my_file.dtypes
+            # Сравниваем типы данных
+            if orig_types == my_file_types:
+                print('Проверка на er3. Типы данных столбцов совпадают')
+            
+            try:
+                orig_types == my_file_types
+            except ValueError as er3:
+                print('Возникла следующая ошибка:{er3}'
+                ' Ожидалось:{orig_types}. Сравниваемый:{my_file_types}')
+
+    # Проверка расширения файла
+    def proceessing5(self):
+        try:
+            pd.read_csv('file')
+        except ValueError as er4:
+            print('Возникла следующая ошибка:{er4}')
+
+    def main():
+        
+        payment = Payment()
+        payment.processing1
+        payment.processing2
+        payment.processing3
+        payment.processing4
+
+    if __name__ == "__main__":
+        main()
+
+
---
+++ main.py
@@ -0,0 +1,6 @@
+import ex  
+def main():
+    ex.main()
+
+if __name__=="__main__":
+    main()
